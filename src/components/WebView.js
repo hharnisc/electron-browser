8610beaacf162b170d0c4b46229c0d60d2cd70d6
Harrison Harnisch <hharnisc@gmail.com>
2017-04-30 16:29:13 -0500

    handle start and stop loading page

--- src/components/WebView.js
+++ src/components/WebView.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const WebView = ({ src }) =>
+const webViewInit = ({
+  webview,
+  onStartLoading,
+  onStopLoading,
+}) => {
+  webview.addEventListener('did-start-loading', onStartLoading);
+  webview.addEventListener('did-stop-loading', onStopLoading);
+};
+
+const WebView = ({
+  src,
+  onStartLoading,
+  onStopLoading,
+}) =>
   <webview
     style={{
       height: '100%',
     }}
     src={src}
+    ref={webview => webViewInit({
+      webview,
+      onStartLoading,
+      onStopLoading,
+    })}
   />;
 
 WebView.propTypes = {
   src: PropTypes.string.isRequired,
+  onStartLoading: PropTypes.func.isRequired,
+  onStopLoading: PropTypes.func.isRequired,
 };
 
 export default WebView;
--- src/containers/WebView.js
+++ src/containers/WebView.js
@@ -2,7 +2,11 @@ import { connect } from 'react-redux';
 import WebView from '../components/WebView';
 
 const mapStateToProps = state => state;
-
+const mapDispatchToProps = dispatch => ({
+  onStartLoading: () => console.log('started loading page'),
+  onStopLoading: () => console.log('stopped loading page'),
+});
 export default connect(
   mapStateToProps,
+  mapDispatchToProps,
 )(WebView);
